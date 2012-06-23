3cfc5b15a3f20a2ae3369ad251a5aca3ea1c8759
Ludovic LEGENDART <ludovic.legendart@green-conseil.com>
2012-06-23 04:06:53 +0200

    adds setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='dbsettings',
+      version='1.0.1',
+      description='django-values to set persisted values as options for your django projects, apps or models',
+      author='RedLion973',
+      packages=['dbsettings'],
+     )
